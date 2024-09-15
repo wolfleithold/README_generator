f2275eee853e777f9fb46e14ba6a27e05ef2c24b
Wolfgang Leithold <wolfleithold@gmail.com>
2024-09-15 14:58:29 -0700

    added Apache 2.0 and BSD 3-Clause License generation, as well as GPLv3

--- utils/licenses.js
+++ utils/licenses.js
@@ -20,23 +20,121 @@ const licenses = {
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.`,
+
   GPLv3: `GNU GENERAL PUBLIC LICENSE
-  Version 3, 29 June 2007
-  
-  ...
-  
-  [Include the entire GPLv3 license text here]`,
+Version 3, 29 June 2007
+
+Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org>
+Everyone is permitted to copy and distribute verbatim copies
+of this license document, but changing it is not allowed.
+
+Preamble
+The GNU General Public License is a free, copyleft license for software and other kinds of works. The licenses for most software are designed to take away your freedom to share and change it. By contrast, the GNU General Public License is intended to guarantee your freedom to share and change all versions of a program—to make sure it remains free software for all its users.
+
+When we speak of free software, we are referring to freedom, not price. Our General Public Licenses are designed to make sure that you have the freedom to distribute copies of free software (and charge for this service if you wish), that you receive source code or can get it if you want it, that you can change the software or use pieces of it in new free programs, and that you know you can do these things.
+
+To protect your rights, we need to prevent others from denying you these rights or asking you to surrender the rights. Therefore, you have certain responsibilities if you distribute copies of the software, or if you modify it. For example, you must pass on the same freedoms to the recipients that you received. You must make sure that everyone else also receives or can get the source code. And you must show them these terms so they know their rights.
+
+Developers that use the GPL protect your rights with two steps: (1) assert copyright on the software, and (2) offer this License giving everyone else these rights.
+
+You also need to make sure that everyone else gets a copy of this License along with the software. And you must keep intact all notices that refer to this License and to the absence of any warranty. Finally, you must make sure that everyone else gets the source code or can get it if they want it.
+
+This License applies to any program or other work which contains a notice placed by the copyright holder saying it may be distributed under the terms of this General Public License. The "Program," below, refers to any such program or work, and a "work based on the Program" means either the Program or any derivative work under copyright law: that is to say, a work containing the Program or a portion of it, either verbatim or with modifications, that is based on the Program.
+
+To the extent that any portion of this License can be interpreted as a warranty, the warranty disclaimers and limitations on liability in Sections 15 and 16 apply.
+
+1. Source Code.
+   The source code for a work means the preferred form of the work for making modifications to it. For an executable work, complete source code means all the source code for all modules it contains, plus any associated interface definition files, plus the scripts used to control compilation and installation of the executable. However, as a special exception, the source code distributed does not include the source code for modules that are standard components of the operating system on which the executable runs, or for common libraries or compiled programs used by the executable, unless the executable itself includes the source code.
+
+2. Basic Permissions.
+   You may copy and distribute verbatim copies of the Program's source code as you receive it, in any medium, provided that you conspicuously and appropriately publish on each copy an appropriate copyright notice and disclaimer of warranty; keep intact all the notices that refer to this License and to the absence of any warranty; and give any other recipients of the Program a copy of this License along with the Program.
+
+3. Copyleft.
+   You may copy and distribute a modified version of the Program under the terms of Section 2 above, provided that you also meet all of these requirements:
+   - The modified work must itself be a free software; and
+   - You must cause the modified files to carry prominent notices stating that you changed the files and the date of any change; and
+   - You must offer all recipients of the Program a copy of this License along with the Program.
+
+4. Combining Works.
+   You may combine the Program with other programs, and distribute the combination under the terms of this License, provided that you meet all the requirements of this License for the Program. This means that the entire combination must be licensed under this License, and you must give the recipients of the combined work a copy of this License.
+
+5. Distribution of Non-source Forms.
+   You may distribute the Program in object code or executable form under the terms of Section 2 above, provided that you also include the source code or make it available, as described in Section 3 above.
+
+6. Disclaimer of Warranty.
+   There is no warranty for the Program. The Program is distributed "as is" without any warranties of any kind, either express or implied, including but not limited to the implied warranties of merchantability or fitness for a particular purpose. 
+
+7. Limitation of Liability.
+   In no event shall the authors or copyright holders be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the Program or the use or other dealings in the Program.
+
+8. Termination.
+   You may not use the Program if you do not comply with the terms of this License. If you fail to comply, your license will be terminated.
+
+9. Acceptance.
+   By copying, modifying, or distributing the Program, you accept the terms of this License.
+
+End of Terms and Conditions.`,
+
   "Apache 2.0": `Apache License
-  Version 2.0, January 2004
-  
-  ...
-  
-  [Include the entire Apache 2.0 license text here]`,
+Version 2.0, January 2004
+http://www.apache.org/licenses/
+
+TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+1. Definitions.
+   a. "License" shall mean the terms and conditions for use, reproduction, and distribution as defined by Sections 1 through 9 of this document.
+   b. "Licensor" shall mean the copyright owner or entity authorized by the copyright owner that is granting the License.
+   c. "You" (or "Your") shall mean an individual or Legal Entity exercising permissions granted by this License.
+   d. "Source" form shall mean the preferred form for making modifications to the Work.
+   e. "Object" form shall mean any form resulting from mechanical transformation or translation of a Source form, including but not limited to compiled code, generated documentation, and conversions to other formats.
+   f. "Work" shall mean the work of authorship, whether in Source or Object form, made available under the License.
+   g. "Derivative Works" shall mean any work, whether in Source or Object form, that is based on (or derived from) the Work and has been modified to produce an altered version of the Work.
+
+2. Grant of Copyright License.
+   Subject to the terms and conditions of this License, each Licensor hereby grants to You a perpetual, worldwide, non-exclusive, no-charge, royalty-free, irrevocable copyright license to reproduce, prepare derivative works of, publicly display, publicly perform, sublicense, and distribute the Work and such derivative works in Source or Object form.
+
+3. Grant of Patent License.
+   Subject to the terms and conditions of this License, each Licensor hereby grants to You a perpetual, worldwide, non-exclusive, no-charge, royalty-free, irrevocable (except as stated in this section) patent license to make, have made, use, offer to sell, sell, import, and otherwise transfer the Work, where such license applies only to those patent claims licensable by such Licensor that are necessarily infringed by their Contribution(s) alone or by combination of their Contribution(s) with the Work.
+
+4. Redistribution.
+   You may reproduce and distribute copies of the Work or Derivative Works thereof in any medium, with or without modifications, and in Source or Object form, provided that you meet the following conditions:
+   a. You must provide a copy of this License along with the Work.
+   b. You must cause any modified files to carry prominent notices stating that You changed the files.
+   c. You must not use the trademarks, service marks, or other identifiers of Licensor, except as required to comply with Section 4b.
+
+5. Submission of Contributions.
+   Unless You explicitly state otherwise, any Contribution submitted for inclusion in the Work shall be under the terms of the License.
+
+6. Trademarks.
+   This License does not grant permission to use the trade names, trademarks, service marks, or product names of Licensor, except as required for compliance with the License.
+
+7. Disclaimer of Warranty.
+   The Work is licensed on an "AS IS" basis, without warranties or conditions of any kind.
+
+8. Limitation of Liability.
+   In no event and under no legal theory, whether in tort (including negligence), contract, or otherwise, unless required by applicable law, will any Licensor be liable for any damages, including consequential damages, arising from the use or inability to use the Work.
+
+9. Accepting the License.
+   By using, copying, modifying, or distributing the Work, you accept and agree to be bound by the terms of this License.
+
+End of License. `,
+
   "BSD 3-Clause": `BSD 3-Clause License
-  
-  ...
-  
-  [Include the entire BSD 3-Clause license text here]`,
+Copyright (c) [year], [fullname]. All rights reserved.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions, and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+Disclaimer
+The software is provided "as is," without warranty of any kind, express or implied, including but 
+not limited to the warranties of merchantability, fitness for a particular purpose, and noninfringement. 
+In no event shall the authors or copyright holders be liable for any claim, damages, or other liability, 
+whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the 
+software or the use or other dealings in the software. `,
 };
 
 module.exports = licenses;
